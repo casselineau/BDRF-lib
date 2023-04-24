b76e004c9c831740c74d6a32999758effea63095
casselineau <charles-alexis.asselineau@anu.edu.au>
2023-04-24 17:06:52 +0200

    Original commit.

---
+++ BRDF_plots.py
@@ -0,0 +1,38 @@
+import numpy as N
+from spectral_processing import wavelength_to_rgb
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+def plot_inplane_BRDF(wavelength, thetas_r_deg, inc_angle_deg, bdrfs, label, diffuse_ratio=False, normalised=False):
+	'''
+	Plots the BDRF in the plane of incidence.
+	This function only makes the plot in an active pyplot figure, the figure creation and saving is done extrenally, in the script dedicated to data processing.
+	Arguments:
+	- wavelength: (this argument is only used here to color the lines with a realistic color using the wavelength_to_rb_function) two options
+		- A single value for teh exact wavelenghth of interest
+		- a list of two values determining the range of the data
+	- thetas_r_deg: reflected polar angles in degrees. Negative values are understood.
+	- inc_angle_deg: angle of incidence of the beam in degrees.
+	- bdrfs: bi-directional reflectance measurements corresponding to this incident angle and reflected angles.
+	- label: label of the bdrf curve.
+	- diffuse_ratio: an optional argument. If set to True, integrates teh arcual BRDF evaluates how "diffuse" the in-plance bdrf is compared to the equivalent diffuse reflectance.
+	- normalised: optional argument, if set to True, the bdrf is normalised to the maximum value measured.
+	'''
+	thetas, inc_angle = thetas_r_deg/180.*N.pi, inc_angle_deg/180.*N.pi
+
+	if diffuse_ratio:
+		dar = N.trapz(bdrfs*N.cos(thetas), thetas) # directional-arcuate reflectance
+		brdf_diff = dar/2. # integration over [-pi2, pi/2] of cos(t)dt, the arc, leads to this
+		bdrfs /= brdf_diff # ratio: over 1 is higher than equivalent diffuse, under 1 is lower.
+	if normalised:
+		bdrfs /= N.amax(bdrfs)
+
+	bdrf_int = interp1d(thetas, bdrfs) # makes the plot look better by interpolating lineraly between angles. Otherwise, the plot will make a straight line in polar coordinates, which is wrong.
+	thetas_plot = N.linspace(N.amin(thetas), N.amax(thetas), 10*len(thetas)+1)
+
+	plt.plot(thetas_plot, bdrf_int(thetas_plot), color=wavelength_to_rgb(wavelength), zorder=100, label=str(label))
+	plt.gca().set_theta_offset(N.pi/2.)
+	plt.gca().set_theta_direction(-1)
+
+	plt.xticks(N.linspace(-N.pi/2., N.pi/2., 19))
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # BDRF-lib
-Scripts used to load and plot Bi-Directionbal Reflection Functions.
+Scripts used to load and plot Bi-Directional Reflection Functions.
+
+Dependencies:
+- Numpy, Scipy, matplotlib
+
+This repository regroups data filtering, interpretation and plotting scripts generated in the H2020 MSCA IF HEASeRS project (https://cordis.europa.eu/project/id/101027316).
+
